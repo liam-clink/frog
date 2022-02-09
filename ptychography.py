c231d90340e288a7eac6724dd8705fe726fba6ec
Liam Clink <clink.6@osu.edu>
2022-02-08 22:08:43 -0500

    First implementation of ptychography

--- ptychography.py
+++ ptychography.py
@@ -1,3 +1,4 @@
+from operator import index
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate
@@ -30,13 +31,14 @@ grid_size = len(original_frequencies) #TODO: throw error if odd grid size, or gr
 duration = grid_size*timestep 
 times = np.fft.ifftshift(np.fft.fftfreq(grid_size, 1./duration))
 shifted_original_frequencies = np.fft.ifftshift(np.fft.fftfreq(grid_size, timestep))
-initial_guess = np.fft.ifft(original_spectral_amplitude)
+initial_scale = 8. # Change to make initial SHG have similar scale to trace
+initial_guess = initial_scale*np.fft.ifftshift(np.fft.ifft(original_spectral_amplitude))
 
 fig, axes = plt.subplots(1, 2)
 axes[0].plot(shifted_original_frequencies, original_spectral_amplitude)
 axes[0].set_title('Original Spectrum')
-axes[1].plot(times, np.fft.ifftshift(initial_guess.real))
-axes[1].plot(times, np.fft.ifftshift(initial_guess.imag))
+axes[1].plot(times, initial_guess.real)
+axes[1].plot(times, initial_guess.imag)
 axes[1].set_title('Flat Spectral Phase Pulse')
 plt.show()
 
@@ -54,24 +56,56 @@ for i in range(trace.shape[-1]):
     plt.plot(shifted_frequencies, trace[i,:])
 plt.show()
 '''
-threshold = 1.5e-3
+threshold = 1.5e-3    
 
 iterations = 5
-
+pulse = initial_guess
 indices = np.arange(len(delays))
 for i in range(iterations):
+    fig, axes = plt.subplots(1, 2)
+    axes[0].plot(times, pulse.real)
+    axes[0].plot(times, pulse.imag)
     rng.shuffle(indices)
     # Iterate through lines
     print(indices)
     for j in indices:
-        print(i,j)
         # Calculate SHG
+        pulse_interpolator = scipy.interpolate.interp1d(times, pulse, bounds_error=False, fill_value=0.)
+        gate_pulse = pulse_interpolator(times-delays[j])
+        shg = pulse*gate_pulse
+        
         # FFT(SHG)
-        # Determine indices above threshold
+        shg_fft = np.fft.fftshift(np.fft.fft(shg))
+        #axes[0].plot(shifted_frequencies, trace[j,:])
+        #axes[1].plot(shifted_frequencies, shg_fft*np.conj(shg_fft))
+        
         # Update the part above threshold
-        # Zero the part below the threshold
+        index_filter = trace[j,:] >= threshold
+        shg_fft[index_filter] = abs(trace[j,index_filter])**0.5*np.exp(1.0j*np.angle(shg_fft[index_filter]))
+        #axes[0].plot(shifted_frequencies[index_filter], trace[j,index_filter])
+        #axes[1].plot(shifted_frequencies[index_filter], abs(shg_fft[index_filter])**2)
+        
+        # Soft threshold the weak part
+        index_filter = trace[j,:] < threshold
+        shg_fft[index_filter].real = np.where(abs(shg_fft[index_filter].real)<1.e-3, 0., shg_fft[index_filter].real)
+        shg_fft[index_filter].imag = np.where(abs(shg_fft[index_filter].imag)<1.e-3, 0., shg_fft[index_filter].imag)
+        #axes[0].plot(shifted_frequencies, trace[j,:])
+        #axes[1].plot(shifted_frequencies, shg_fft.real)
+        #axes[1].plot(shifted_frequencies, shg_fft.imag)
+        
         # IFFT(SHG)
+        shg_new = np.fft.ifft(np.fft.ifftshift(shg_fft))
+        #axes[0].plot(times, abs(shg)**2)
+        #axes[1].plot(times, abs(shg_new)**2)
+
         # Update E
+        alpha = rng.uniform(0.1, 0.5)
+        scale = alpha*np.conj(gate_pulse)/(gate_pulse*np.conj(gate_pulse) + 1.e-3)
+        pulse += scale*(shg_new - shg)
+    
+    axes[1].plot(times, pulse.real)
+    axes[1].plot(times, pulse.imag)
+    plt.show()
 
 #TODO: add error if shifted_frequencies != shifted_original frequencies
 #plt.pcolormesh(shifted_frequencies, delays, trace)
