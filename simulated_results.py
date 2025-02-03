7a4c16209cef5a64386bd30cbdc913f07715f79b
liam <clink.6@osu.edu>
2025-02-03 14:06:50 -0500

    Added Simulation

---
+++ simulated_results.py
@@ -0,0 +1,64 @@
+"""Simulate autocorrelation for gaussian beams
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+WIDTH = 10.0e-3
+HEIGHT = 10.0e-3
+DEPTH = 1.0e-3
+C = 2.99e8
+
+
+def gaussian_pulse(
+    x,
+    y,
+    z,
+    w: float,
+    delay: float,
+    duration: float,
+    angle: float,
+    x_offset: float,
+    y_offset: float,
+):
+    x_transformed = np.cos(angle) * x + np.sin(angle) * z
+    z_transformed = -np.sin(angle) * x + np.cos(angle) * z
+
+    exponent = -(((z_transformed / C - delay) / duration) ** 2) / 2
+    exponent += -(((x_transformed - x_offset) / w) ** 2)
+    exponent += -(((y - y_offset) / w) ** 2)
+    return np.exp(exponent)
+
+
+x = np.linspace(-WIDTH / 2, WIDTH / 2, 200)
+y = np.linspace(-HEIGHT / 2, HEIGHT / 2, 200)
+z = np.linspace(0, DEPTH, 200)
+
+X, Z = np.meshgrid(x, z)
+
+beam_1 = gaussian_pulse(
+    X,
+    0.0,
+    Z,
+    w=2.0e-3,
+    delay=500.0e-15,
+    duration=250.0e-15,
+    angle=10 / 180 * np.pi,
+    x_offset=0.0,
+    y_offset=0.0,
+)
+beam_2 = gaussian_pulse(
+    X,
+    0.0,
+    Z,
+    w=2.0e-3,
+    delay=500.0e-15,
+    duration=250.0e-15,
+    angle=-10 / 180 * np.pi,
+    x_offset=0.0,
+    y_offset=0.0,
+)
+
+plt.pcolormesh(X, Z, beam_1 + beam_2)
+plt.gca().set_aspect("equal")
+plt.show()
